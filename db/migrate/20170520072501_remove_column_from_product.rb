4bc4d385d7e9652c12d5bfd457be364ea979861d
SegueX <free1989xj@hotmail.com>
2017-05-20 22:58:37 +0800

    implement admin/orders show function

--- db/migrate/20170520072501_remove_column_from_product.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromProduct < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :products, :category, :string
-  end
-end
--- db/migrate/20170520073120_remove_column_from_cart_item.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromCartItem < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :cart_items, :category, :string
-  end
-end
